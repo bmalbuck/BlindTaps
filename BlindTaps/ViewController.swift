303e7b742e201770ee08950facb9b3adccd80746
Brian Buck <bbuck911@gmail.com>
2017-01-27 10:33:43 -0800

    Initial Commit

---
+++ BlindTaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlindTaps
+//
+//  Created by Brian Buck on 1/27/17.
+//  Copyright © 2017 Brian Buck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
